d44edb1a0384cd0b327a75362173c9e934992970
이정현 <jay6423@gmail.com>
2025-06-30 18:24:17 +0900

    Update mainApi.js

--- src/common/api/mainApi.js
+++ src/common/api/mainApi.js
@@ -1,8 +1,8 @@
 import {addAuthHeader, handleAuthError} from '../util/jwtUtil';
 import axios from 'axios';
 
-export const API_SERVER_HOST = "http://localhost:8080"; // 개발용 주소
-// export const API_SERVER_HOST = "https://onepm-2025.shop"; // 배포용 주소
+// export const API_SERVER_HOST = "http://localhost:8080"; // 개발용 주소
+export const API_SERVER_HOST = "https://onepm-2025.shop"; // 배포용 주소
 
 
 const axiosInstance = axios.create({
